bf8f7fe8d6fbecdbacb34e36a5ed83baa556c2ba
ZhaoJiaLiang <zhaojialiang@sogou-inc.com>
2019-07-25 00:39:25 +0800

    add create matrix function

--- VideoPlayer.cpp
+++ VideoPlayer.cpp
@@ -1,26 +1,24 @@
-#include "highgui.h"
+#include "opencv2/opencv.hpp"
+using namespace cv;
 
-int main ( int argc, char** argv) {
-    cvNamedWindow( "Excample2", CV_WINDOW_AUTOSIZE);
-    CvCapture* capture = cvCreateFileCapture( argv[1] );
-    IplImage* frame;
+int main(int, char**)
+{
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
 
-    while(1) {
-        frame = cvQueryFrame( capture );
-        if (!frame)
-        {
-            break;
-        }
-        
-        cvShowImage("Example2", frame);
-        char c = cvWaitKey(33);
-        if ( c == 27)
-        {   
-            break;
-        }
-        
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, COLOR_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
     }
-    cvReleaseCapture( &capture );
-    cvDestroyWindow ( "Example2" );
-
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 }
