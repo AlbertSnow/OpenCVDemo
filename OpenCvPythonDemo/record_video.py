843bab987b5a0a81a3ef363d47902626d92d600e
AlbertSnow <Albert.Snow.Email@gmail.com>
2019-07-30 08:21:07 +0800

    fix can't record video bug

--- OpenCvPythonDemo/record_video.py
+++ OpenCvPythonDemo/record_video.py
@@ -15,7 +15,7 @@ STD_DIMENSION = {
     "480p" : (640, 480),
     "720p" : (1280, 720),
     "1080p" : (1920, 1080),
-    "4K":   (3340, 2160)
+    "4K":   (3340, 2160),
 }
 
 def get_dims(cap, res='1080p'):
@@ -27,7 +27,7 @@ def get_dims(cap, res='1080p'):
 
 VIDEO_TYPE = {
     'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID')
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
 }
 
 def get_video_type(filename) :
@@ -40,13 +40,18 @@ cap = cv2.VideoCapture(0)
 dims = get_dims(cap, res=my_res)
 vidoe_type_cv2 = get_video_type(filename)
 
-out = cv2.VideoWriter(filename, vidoe_type_cv2, frames_per_seconds, dims)
+out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('M','J','P','G'), frames_per_seconds, dims)
+# out = cv2.VideoWriter(filename,  cv2.VideoWriter_fourcc('M','J','P','G'), frames_per_seconds, (1280, 720))
 
+print ("this is a tuple: %s" %(dims,))
+
+change_res(cap, 1280, 720)
 
 while(True):
     ret, frame = cap.read()
     out.write(frame)
 
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
             break
